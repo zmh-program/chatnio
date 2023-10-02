becb813cf74c520579013a993d97085077e948c4
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-10-02 21:24:32 +0800

    add cors for open api

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,10 +5,27 @@ import (
 	"chat/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// open api for all origins
+		if strings.HasPrefix(c.Request.URL.Path, "/v1") {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+			if c.Request.Method == "OPTIONS" {
+				c.Writer.Header().Set("Access-Control-Max-Age", "7200")
+				c.AbortWithStatus(http.StatusOK)
+				return
+			}
+			c.Next()
+			return
+		}
+
 		origin := c.Request.Header.Get("Origin")
 		if utils.Contains(origin, globals.AllowedOrigins) {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)

24c8c9e0594e214023220834928babcc58164bd1
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-11-01 23:15:32 +0800

    add gpt-4v and dalle3 models

--- app/src/conf.ts
+++ app/src/conf.ts
@@ -8,7 +8,7 @@ import {
 } from "@/utils/env.ts";
 import { getMemory } from "@/utils/memory.ts";
 
-export const version = "3.6.6";
+export const version = "3.6.7";
 export const dev: boolean = getDev();
 export const deploy: boolean = true;
 export let rest_api: string = getRestApi(deploy);
@@ -19,16 +19,26 @@ export const supportModels: Model[] = [
   // openai models
   { id: "gpt-3.5-turbo-0613", name: "GPT-3.5", free: true, auth: false },
   { id: "gpt-3.5-turbo-16k-0613", name: "GPT-3.5-16k", free: true, auth: true },
-  { id: "gpt-4", name: "GPT-4", free: false, auth: true },
-  { id: "gpt-4-32k", name: "GPT-4-32k", free: false, auth: true },
+  { id: "gpt-4-0613", name: "GPT-4", free: false, auth: true },
+  { id: "gpt-4v", name: "GPT-4-Vision", free: false, auth: true },
+  { id: "gpt-4-dalle", name: "GPT-4-DALLE3", free: false, auth: true },
+  { id: "gpt-4-32k-0613", name: "GPT-4-32k", free: false, auth: true },
 
   // spark desk
-  { id: "spark-desk-v2", name: "讯飞星火 V2", free: false, auth: true },
-  { id: "spark-desk-v3", name: "讯飞星火 V3", free: false, auth: true },
+  { id: "spark-desk-v3", name: "讯飞星火 V3", free: true, auth: true },
+  { id: "spark-desk-v2", name: "讯飞星火 V2", free: true, auth: true },
+  { id: "spark-desk-v2", name: "讯飞星火 V1.5", free: true, auth: true },
 
   // anthropic models
   { id: "claude-1", name: "Claude-2", free: true, auth: false },
   { id: "claude-2", name: "Claude-2-100k", free: false, auth: true }, // not claude-2-100k
+
+  // dashscope models
+  { id: "qwen-plus-net", name: "通义千问 Plus X", free: false, auth: true },
+  { id: "qwen-plus", name: "通义千问 Plus", free: false, auth: true },
+  { id: "qwen-turbo-net", name: "通义千问 Turbo X", free: false, auth: true },
+  { id: "qwen-turbo", name: "通义千问 Turbo", free: false, auth: true },
+
 
   // google palm2
   { id: "chat-bison-001", name: "Palm2", free: true, auth: true },
@@ -40,12 +50,6 @@ export const supportModels: Model[] = [
   { id: "zhipu-chatglm-pro", name: "ChatGLM Pro", free: false, auth: true },
   { id: "zhipu-chatglm-std", name: "ChatGLM Std", free: false, auth: true },
   { id: "zhipu-chatglm-lite", name: "ChatGLM Lite", free: true, auth: true },
-
-  // dashscope models
-  { id: "qwen-plus-net", name: "通义千问 Plus X", free: false, auth: true },
-  { id: "qwen-plus", name: "通义千问 Plus", free: false, auth: true },
-  { id: "qwen-turbo-net", name: "通义千问 Turbo X", free: false, auth: true },
-  { id: "qwen-turbo", name: "通义千问 Turbo", free: false, auth: true },
 ];
 
 export function login() {

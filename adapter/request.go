460f87b09159b5a49e74033aa9d12a699e8539cc
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-11-21 13:03:32 +0800

    fix

--- adapter/request.go
+++ adapter/request.go
@@ -2,18 +2,25 @@ package adapter
 
 import (
 	"chat/globals"
-	"chat/utils"
 )
 
 func IsAvailableError(err error) bool {
 	return err != nil && err.Error() != "signal"
 }
 
+func getRetries(retries *int) int {
+	if retries == nil {
+		return defaultMaxRetries
+	}
+
+	return *retries
+}
+
 func NewChatRequest(props *ChatProps, hook globals.Hook) error {
 	err := createChatRequest(props, hook)
 
 	props.Current++
-	retries := utils.Multi(props.MaxRetries == nil, defaultMaxRetries, *props.MaxRetries)
+	retries := getRetries(props.MaxRetries)
 
 	if IsAvailableError(err) && props.Current < retries {
 		return NewChatRequest(props, hook)

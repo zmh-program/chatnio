fd90e8806afeaaa8895137f8003c39e81a8d31f3
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-11-27 18:36:56 +0800

    fix

--- app/src/components/ReloadService.tsx
+++ app/src/components/ReloadService.tsx
@@ -1,29 +1,11 @@
-import { useRegisterSW } from "virtual:pwa-register/react";
 import { version } from "@/conf.ts";
 import { useTranslation } from "react-i18next";
 import { useToast } from "./ui/use-toast.ts";
-import { useEffect } from "react";
-import { ToastAction } from "./ui/toast.tsx";
 import { getMemory, setMemory } from "@/utils/memory.ts";
 
 function ReloadPrompt() {
   const { t } = useTranslation();
   const { toast } = useToast();
-
-  const {
-    offlineReady: [offlineReady, setOfflineReady],
-    needRefresh: [needRefresh, setNeedRefresh],
-    updateServiceWorker,
-  } = useRegisterSW({
-    onRegisteredSW() {
-      console.debug(`[service] service worker registered (version ${version})`);
-    },
-    onRegisterError(error) {
-      console.log(
-        `[service] service worker registration failed: ${error.message}`,
-      );
-    },
-  });
 
   const before = getMemory("version");
   if (before.length > 0 && before !== version) {
@@ -37,33 +19,6 @@ function ReloadPrompt() {
     );
   }
   setMemory("version", version);
-
-  useEffect(() => {
-    if (offlineReady) {
-      toast({
-        title: t("service.offline-title"),
-        description: t("service.offline"),
-      });
-    }
-
-    if (needRefresh) {
-      toast({
-        title: t("service.title"),
-        description: t("service.description"),
-        action: (
-          <ToastAction
-            altText={t("service.update")}
-            onClick={() => updateServiceWorker(true)}
-          >
-            {t("service.update")}
-          </ToastAction>
-        ),
-      });
-
-      setOfflineReady(false);
-      setNeedRefresh(false);
-    }
-  }, [offlineReady, needRefresh]);
 
   return <></>;
 }
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,41 +9,6 @@ export default defineConfig({
     react(),
     createHtmlPlugin({
       minify: true,
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        id: "chatnio",
-        name: "Chat Nio",
-        short_name: "Chat Nio",
-        description: "👋 Chat Nio, lightweight Web ChatGPT chat site",
-        theme_color: "#0a0a0a",
-        icons: [{
-          src: "/logo.png",
-          sizes: "64x64",
-          type: "image/png",
-        }],
-        start_url: "/",
-      },
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-        cleanupOutdatedCaches: true,
-        globPatterns: [
-          "**/*.{js,css,png,jpg,jpeg,svg,ico,webp,woff,woff2,ttf,eot,txt,xml,json}",
-        ],
-        runtimeCaching: [{
-          urlPattern: new RegExp('^https://open.lightxi.com/'),
-          handler: "CacheFirst",
-          options: {
-            cacheName: "lightxi-cdn",
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365,
-            }
-          }
-        }],
-      },
     }),
   ],
   resolve: {

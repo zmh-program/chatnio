07e2d887c833cd7a91b850c8174850d72eeb781e
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-03-14 10:31:36 +0800

    chore: update package dialog

--- app/src/dialogs/PackageDialog.tsx
+++ app/src/dialogs/PackageDialog.tsx
@@ -75,7 +75,7 @@ function PackageDialog() {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button className={`mt-2`} variant={`outline`} onClick={() => dispatch(closeDialog())}>
+          <Button className={`mt-2 sm:mt-0`} variant={`outline`} onClick={() => dispatch(closeDialog())}>
             {t("pkg.close")}
           </Button>
           <Button

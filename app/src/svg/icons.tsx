e7c1d3bc8a3b60eba47b34b5672b8c43658e5429
Ethan Stewart <ethanste2023@gmail.com>
2024-04-20 08:53:47 +0800

    style: attribute stroke-linecap is not allowed, changed to strokeLinecap

--- app/src/svg/icons.tsx
+++ app/src/svg/icons.tsx
@@ -181,13 +181,13 @@ export const ArrowCircleDown = ({ style }: { style?: CSSProperties }) => {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <circle cx="12" cy="12" r="11" stroke="#1976d2" stroke-width="2" />
+            <circle cx="12" cy="12" r="11" stroke="#1976d2" strokeWidth="2" />
             <path
                 d="M17 13L12 18L7 13M12 6L12 17"
                 stroke="#1976d2"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             ></path>
         </svg>
     );
